c04a168782d996d356b6b4c71fb23117f95922d2
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-05-01 15:26:44 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,79 @@
+// 1й спосіб
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const CHANGE_COLOR_DELAY = 1000;
+
+let intervalId = null;
+let isBtnActive = startBtn.disabled = false;
+let isBtnDisable = stopBtn.disabled = true;
+
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+
+function onStartBtnClick() {
+    if (isBtnActive) {
+        return;
+    };
+    
+    isBtnActive = startBtn.disabled = true;
+    isBtnDisable = stopBtn.disabled = false;
+    
+    intervalId = setInterval(callbackSetColor, CHANGE_COLOR_DELAY);
+};
+
+function onStopBtnClick() {
+    isBtnActive = startBtn.disabled = false;
+    isBtnDisable = stopBtn.disabled = true;
+
+    clearInterval(intervalId);
+};
+
+function callbackSetColor() {
+    return body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+// 2й спосіб (відмінність лише в способі додавання до елемента атрибуту disabled)
+// const body = document.querySelector('body');
+// const startBtn = document.querySelector('[data-start]');
+// const stopBtn = document.querySelector('[data-stop]');
+// const CHANGE_COLOR_DELAY = 1000;
+// let intervalId = null;
+
+// startBtn.addEventListener('click', onStartBtnClick);
+// stopBtn.addEventListener('click', onStopBtnClick);
+
+// stopBtn.setAttribute('disabled', '');
+
+// function onStartBtnClick() {
+//     if (startBtn.hasAttribute('disabled')) {
+//         return;
+//     };
+    
+//     stopBtn.removeAttribute('disabled');
+//     startBtn.setAttribute('disabled', '');
+    
+//     intervalId = setInterval(callbackSetColor, CHANGE_COLOR_DELAY);
+// };
+
+// function onStopBtnClick() {
+//     startBtn.removeAttribute('disabled');
+//     stopBtn.setAttribute('disabled', '');
+
+//     clearInterval(intervalId);
+// };
+
+// function callbackSetColor() {
+//     return body.style.backgroundColor = getRandomHexColor();
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
